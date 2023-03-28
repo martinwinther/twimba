4c2cc2e9b89c55c24028fd2323694d9dc5546ef6
martinwinther <mswinther@icloud.com>
2023-03-28 10:10:26 +0200

    Added data rendering

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 import { tweetsData } from './data';
 
-console.log(tweetsData);
+const tweetInput = document.getElementById('tweet-input');
+const tweetBtn = document.getElementById('tweet-btn');
+
+tweetBtn.addEventListener('click', function () {
+  console.log(tweetInput.value);
+});
+
+function getFeedHtml() {
+  let feedHtml = ``;
+  tweetsData.forEach(function (tweet) {
+    feedHtml += `
+    <div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">T${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>`;
+  });
+  return feedHtml;
+}
+
+function renderTweets() {
+  document.getElementById('feed').innerHTML = getFeedHtml();
+}
+
+renderTweets();
+/*
+
+
+*/
